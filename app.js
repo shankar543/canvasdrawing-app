a8d96be498f0d04f1c41205175546e90b228193d
shankar534 <janapareddivijayashankar@gmail.com>
2023-08-16 21:24:22 +0530

    added some changes

--- app.js
+++ app.js
@@ -5,23 +5,37 @@ let sizedecrease = document.querySelector('#sizedecrease');
 let colorEL=document.querySelector('#color');
 let sizeEL= document.querySelector("#size")
 let x = 0,
-	y = 0;
+    y = 0;
+let x2 = 0, y2 = 0 ,x1=0,y1=0;
 let size = 30;
 let color='green'
 let ctx = canvas.getContext("2d");
 
 let ismouseDown = false;
 canvas.onmousedown = function (e) {
-	ismouseDown = true;
+    ismouseDown = true;
+    x = e.offsetX;
+    y = e.offsetY;
+    x1 = x;
+    y1 = y;
+    drawCircle();
+    // drawCircle();
 };
 canvas.addEventListener("mouseup", function (e) {
-	ismouseDown = false;
+    ismouseDown = false;
+    x = undefined;
+    y = undefined;
+    x2 = undefined;
+    y2 = undefined;
 });
 canvas.onmousemove = function (e) {
 	if (ismouseDown) {
-		x = e.offsetX;
-		y = e.offsetY;
-		drawCircle();
+		x2 = e.offsetX;
+        y2 = e.offsetY;
+        drawCircle();
+        drawLine();
+        x1 = e.offsetX;
+        y1 = e.offsetY;
 	}
 };
 clear.addEventListener("click", function () {
@@ -30,18 +44,16 @@ ctx.clearRect(0,0,canvas.width,canvas.height)
 
 function drawCircle() {
 ctx.beginPath();
-	ctx.strokeStyle = "red";
-	ctx.arc(x, y, size, 0, Math.PI * 2);
+ ctx.arc(x1, y1, size, 0, Math.PI * 2);
 	ctx.fillStyle = color;
-    ctx.strokeWidth=2
+    ctx.strokeWidth = 4;
 ctx.fill();
-	
 }
 function drawRect() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-drawCircle();
+// drawCircle();
 sizeincrease.addEventListener('click',function(){
 if(size>45){
     size=50
@@ -58,6 +70,35 @@ sizedecrease.addEventListener('click',function(){
     }
     sizeEL.innerHTML = size;
     });
-    colorEL.addEventListener('change',function(e){
-        color=e.target.value;
-    })
+colorEL.addEventListener('change', function (e) {
+    color = e.target.value;
+});
+canvas.addEventListener("touchstart", (e) => {
+    let touch = e.touches[0];
+    x=touch.clientX - canvas.getBoundingClientRect().left;
+    y=touch.clientY - canvas.getBoundingClientRect().top;
+    drawCircle();
+});
+canvas.addEventListener("touchmove", (e) => {
+    let touch = e.touches[0];
+    x = touch.clientX - canvas.getBoundingClientRect().left;
+    y = touch.clientY - canvas.getBoundingClientRect().top;
+    drawCircle();
+});
+canvas.addEventListener("touchstop", (e) => {
+    ismouseDown = false;
+    ctx.closePath();
+    x1 = undefined;
+    y1 = undefined;
+    x2 = undefined;
+    y2 = undefined;
+});
+
+function drawLine() {
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size*2;
+    ctx.moveTo(x1,y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+}
